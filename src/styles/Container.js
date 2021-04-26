de3d4ee392f521dd037331c1592ed787b2e408f5
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-04-26 19:09:24 +0300

    Base Styles

---
+++ src/styles/Container.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+    padding: 0 15%;
+    margin-right: auto;
+    margin-left: auto;
+
+    @media screen and (max-width: 1200px) {
+        padding: 0 10%;
+    }
+
+    @media screen and (max-width: 745px) {
+        padding: 0 5%;
+    }
+`
---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+    colors: {
+        brandColor: '#005FA3',
+        titleColor: '#BDBDBD',
+        desColor: '#333333',
+        textColor: '#000000',
+        navColor: '#222222',
+        bgGray: '#EBEBEB'
+    }
+};
