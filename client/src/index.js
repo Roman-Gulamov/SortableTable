670319f48dd9999a87ff9a95a279450b6b293e82
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-04-28 04:03:35 +0300

    Added imports

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import Main from './Pages/Main';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/fonts/lato/stylesheet.css';
+
+
+render(<Main />, document.getElementById('root'));
